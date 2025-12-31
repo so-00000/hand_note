bf90b993c465a0a1f2122aa8647dc495f7eb64dc
USER <user@USERnoMac-mini.local>
2025-12-31 21:49:03 +0900

    #31

---
+++ ios/MemoHomeWidget/MemoHomeWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  MemoHomeWidgetBundle.swift
+//  MemoHomeWidget
+//
+//  Created by USER on 2025/12/31.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct MemoHomeWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        MemoHomeWidget()
+    }
+}
