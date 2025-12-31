c6f04a00c6e746eb0751d9bbc77c7f197df9b64a
USER <user@USERnoMac-mini.local>
2025-12-31 21:53:32 +0900

    #31

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,8 @@ import 'features/setting_mgmt/2_view_model/settings_view_model.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  
+  await HomeWidget.setAppGroupId('group.com.example.handNote');
 
   // ✅ Cold Start（ホームウィジェット経由で起動）
   final uri = await HomeWidget.initiallyLaunchedFromHomeWidget();
