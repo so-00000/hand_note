0a3a281be853ce24ccfefe6be36233e94b3bbf3c
USER <you@gmail.com>
2026-01-01 22:57:22 +0900

    “dev_41”

---
+++ ios/MemoWidgets/MemoWidgets.swift
@@ -0,0 +1,84 @@
+//
+//  MemoWidgets.swift
+//  MemoWidgets
+//
+//  Created by USER on 2026/01/01.
+//
+
+import WidgetKit
+import SwiftUI
+
+struct Provider: TimelineProvider {
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date(), emoji: "😀")
+    }
+
+    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+        let entry = SimpleEntry(date: Date(), emoji: "😀")
+        completion(entry)
+    }
+
+    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        var entries: [SimpleEntry] = []
+
+        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        let currentDate = Date()
+        for hourOffset in 0 ..< 5 {
+            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+            let entry = SimpleEntry(date: entryDate, emoji: "😀")
+            entries.append(entry)
+        }
+
+        let timeline = Timeline(entries: entries, policy: .atEnd)
+        completion(timeline)
+    }
+
+//    func relevances() async -> WidgetRelevances<Void> {
+//        // Generate a list containing the contexts this widget is relevant in.
+//    }
+}
+
+struct SimpleEntry: TimelineEntry {
+    let date: Date
+    let emoji: String
+}
+
+struct MemoWidgetsEntryView : View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        VStack {
+            Text("Time:")
+            Text(entry.date, style: .time)
+
+            Text("Emoji:")
+            Text(entry.emoji)
+        }
+    }
+}
+
+struct MemoWidgets: Widget {
+    let kind: String = "MemoWidgets"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+            if #available(iOS 17.0, *) {
+                MemoWidgetsEntryView(entry: entry)
+                    .containerBackground(.fill.tertiary, for: .widget)
+            } else {
+                MemoWidgetsEntryView(entry: entry)
+                    .padding()
+                    .background()
+            }
+        }
+        .configurationDisplayName("My Widget")
+        .description("This is an example widget.")
+    }
+}
+
+#Preview(as: .systemSmall) {
+    MemoWidgets()
+} timeline: {
+    SimpleEntry(date: .now, emoji: "😀")
+    SimpleEntry(date: .now, emoji: "🤩")
+}
---
+++ ios/MemoWidgets/MemoWidgetsBundle.swift
@@ -0,0 +1,16 @@
+//
+//  MemoWidgetsBundle.swift
+//  MemoWidgets
+//
+//  Created by USER on 2026/01/01.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct MemoWidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        MemoWidgets()
+    }
+}
